db9f9b63427722e67d07b35348912e0cc6640cbc
popigg <popigg@gmail.com>
2015-08-22 14:38:56 +0200

    back those to the root

---
+++ app/app.js
@@ -0,0 +1,59 @@
+'use strict';
+
+var crmyApp = angular.module('crmy',
+    [
+        'ui.router',
+        'ui.bootstrap',
+        'ngCookies',
+        'crmy.auth',
+        'crmy.customer'
+    ]
+);
+
+crmyApp.config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise("/login");
+
+    $stateProvider
+    .state('login', {
+        url: '/login',
+        views: {
+            'main@' : {
+                controller: 'AuthController',
+                templateUrl: 'app/partials/auth/login.html'
+            }
+        }
+    })
+
+    .state('dashboard', {
+        url: '/dashboard',
+        views: {
+            'main@' : {
+                templateUrl: 'app/partials/dashboard.html'
+            }
+        }
+    })
+
+    .state('customers', {
+        url: '/customers',
+        views: {
+            'main@' : {
+                controller: 'CustomerController',
+                templateUrl: 'app/partials/customer/customerList.html'
+            }
+        }
+    })
+
+    .state('customerDetails', {
+        url: '/customerDetails/:id',
+        views : {
+            'main@' : {
+                controller: 'CustomerDetailsController',
+                templateUrl: 'app/partials/customer/customerDetails.html'
+            }
+        }
+    })
+});
+
+crmyApp.config(function($httpProvider) {
+     $httpProvider.interceptors.push('AuthServiceInterceptor');
+});
